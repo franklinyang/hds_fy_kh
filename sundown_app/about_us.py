b5004710855a222a4bf2a8c2db2e23f07410cc22
Franklin <franklin@firefly.health>
2020-10-18 16:54:23 -0400

    Updating about_us

--- sundown_app/about_us.py
+++ sundown_app/about_us.py
@@ -7,19 +7,22 @@ about_us_html = html.Div(
         html.H1("About us"),
         html.P(
             """
-            This contains a map of the United States that examines Sundown towns. Sundown towns were towns,
-            cities, and municipalities in the United States with anecdoatal evidence of systemic racism.\n
+            This contains a map of the United States that examines Sundown towns.
+            Sundown towns were towns, cities, and municipalities in the United States
+            with anecdoatal evidence of systemic racism.\n
             """
         ),
         html.P(
             """
-            This website embodies the work from a Capstone project from a data sciene masters program at the Harvard Chan School of Public Health.
-            Participants were Kexin Huang and Franklin Yang, advised by Yulin Hwsen and Heather Mattie. \n
+            This website embodies the work from a Capstone project from a data sciene masters
+            program at the Harvard Chan School of Public Health.
+            Participants were Kexin Huang and Franklin Yang, advised by Yulin Hwsen and Heather Mattie.\n
             """
         ),
         html.P(
             """
-            This map was created using data collected from a Sundown Town project completed at Tougaloo college: https://sundown.tougaloo.edu/.
+            This map was created using data collected from a Sundown Town project completed at 
+            Tougaloo college: https://sundown.tougaloo.edu/.\n
             """
         ),
     ]
